00979fc974f5c472ba640a0a05152124120a1e18
konghiran.w <konghiran.w@lmwn.com>
2024-10-26 16:40:16 +0700

    gh action

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Notebook v0.01",
+    pageTitle: "🪴 kidpeterpan.github.io",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
